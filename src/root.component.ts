1b9101a8f0884daf2b39f6a3a8aba758672758aa
stan e <yi.hang@live.com>
2018-03-28 00:32:20 +0800

    启动和路由模块的添加

---
+++ src/root.component.ts
@@ -0,0 +1,25 @@
+import { Injector, HostBinding, Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: `<router-outlet></router-outlet>`
+})
+export class RootComponent implements OnInit {
+    layoutCollapsed: boolean = false;
+    layoutFixed: boolean = true;
+    layoutBoxed: boolean = false;
+    @HostBinding('class.layout-fixed') get isFixed() { return this.layoutFixed; }
+    @HostBinding('class.layout-boxed') get isBoxed() { return this.layoutBoxed; }
+    @HostBinding('class.aside-collapsed') get isCollapsed() { return this.layoutCollapsed; }
+
+    ngOnInit() {
+        // event.on
+        abp.event.on('abp.theme-setting.collapsed', collapsed => {
+            this.layoutCollapsed = collapsed;
+        });
+    }
+
+    constructor(injector: Injector) {
+    }
+
+}
