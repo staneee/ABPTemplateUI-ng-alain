6723c529ff9d4eec5dca9a1a55e795674723b183
stan e <yi.hang@live.com>
2018-03-28 00:10:30 +0800

    添加service-proxies

---
+++ src/shared/service-proxies/service-proxy.module.ts
@@ -0,0 +1,42 @@
+﻿import { NgModule } from '@angular/core';
+
+import * as ApiServiceProxies from './service-proxies';
+
+@NgModule({
+    providers: [
+        ApiServiceProxies.RoleServiceProxy,
+        ApiServiceProxies.SessionServiceProxy,
+        ApiServiceProxies.TenantServiceProxy,
+        ApiServiceProxies.TenantDashboardServiceProxy,
+        ApiServiceProxies.TenantRegistrationServiceProxy,
+        ApiServiceProxies.TenantSettingsServiceProxy,
+        ApiServiceProxies.UserServiceProxy,
+        ApiServiceProxies.UserLinkServiceProxy,
+        ApiServiceProxies.UserLoginServiceProxy,
+        ApiServiceProxies.TokenAuthServiceProxy,
+        ApiServiceProxies.AccountServiceProxy,
+        ApiServiceProxies.ProfileServiceProxy,
+        ApiServiceProxies.PermissionServiceProxy,
+        ApiServiceProxies.AuditLogServiceProxy,
+        ApiServiceProxies.CachingServiceProxy,
+        ApiServiceProxies.ChatServiceProxy,
+        ApiServiceProxies.CommonLookupServiceProxy,
+        ApiServiceProxies.EditionServiceProxy,
+        ApiServiceProxies.FriendshipServiceProxy,
+        ApiServiceProxies.HostDashboardServiceProxy,
+        ApiServiceProxies.HostSettingsServiceProxy,
+        ApiServiceProxies.InstallServiceProxy,
+        ApiServiceProxies.InvoiceServiceProxy,
+        ApiServiceProxies.LanguageServiceProxy,
+        // ApiServiceProxies.LogServiceProxy,
+        ApiServiceProxies.OrganizationUnitServiceProxy,
+        ApiServiceProxies.PaymentServiceProxy,
+        ApiServiceProxies.SubscriptionServiceProxy,
+        ApiServiceProxies.UiCustomizationSettingsServiceProxy,
+        ApiServiceProxies.WebLogServiceProxy,
+        ApiServiceProxies.NotificationServiceProxy,
+        ApiServiceProxies.DemoUiComponentsServiceProxy
+        //ApiServiceProxies.ConfigurationServiceProxy
+    ]
+})
+export class ServiceProxyModule { }
