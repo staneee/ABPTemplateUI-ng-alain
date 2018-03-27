1647320654ef88d19bfeec203d038f5fe6a17313
stan e <yi.hang@live.com>
2018-03-27 22:27:38 +0800

    导入abp相关模块和配置

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,4 +1,10 @@
 /* SystemJS module definition */
+///<reference path="../node_modules/abp-web-resources/Abp/Framework/scripts/abp.d.ts"/>
+///<reference path="../node_modules/abp-web-resources/Abp/Framework/scripts/libs/abp.signalr.d.ts"/>
+///<reference path="../node_modules/moment/moment.d.ts"/>
+///<reference path="../node_modules/@types/moment-timezone/index.d.ts"/>
+///<reference path="../node_modules/@types/toastr/index.d.ts"/>
+
 declare var module: NodeModule;
 interface NodeModule {
   id: string;
