bb52c41033afaa332159f81363628ba9651db952
stan e <yi.hang@live.com>
2018-03-27 23:58:38 +0800

    自定义abp扩展模块

---
+++ src/shared/abp-ex/abp.module.ts
@@ -0,0 +1,62 @@
+﻿import { NgModule  , Injector} from '@angular/core';
+import { HttpModule, JsonpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
+import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from '@abp/message/message.service';
+import { NotifyService } from '@abp/notify/notify.service';
+import { LogService } from '@abp/log/log.service';
+
+import { TokenService } from '@abp/auth/token.service';
+import { PermissionCheckerService } from '@abp/auth/permission-checker.service';
+import { AbpSessionService } from '@abp/session/abp-session.service';
+import { AbpMultiTenancyService } from '@abp/multi-tenancy/abp-multi-tenancy.service';
+import { AbpHttpConfiguration, AbpHttp } from './abpHttp';
+
+import { FeatureCheckerService } from '@abp/features/feature-checker.service';
+import { LocalizationService } from '@abp/localization/localization.service';
+import { SettingService } from '@abp/settings/setting.service';
+
+import { UtilsService } from './utils/utils.service';
+
+export function abpHttpFactory(
+    xhrBackend: XHRBackend,
+    requestOptions: RequestOptions,
+    configuration: AbpHttpConfiguration,
+    injector: Injector): Http {
+    return new AbpHttp(xhrBackend, requestOptions, configuration);
+}
+
+export let ABP_HTTP_PROVIDER = {
+    provide: Http,
+    useFactory: abpHttpFactory,
+    deps: [XHRBackend, RequestOptions, AbpHttpConfiguration]
+};
+
+@NgModule({
+    imports: [
+        HttpModule,
+        JsonpModule
+    ],
+
+    declarations: [
+    ],
+
+    providers: [
+        CookieService,
+        AbpHttpConfiguration,
+        ABP_HTTP_PROVIDER,
+        MessageService,
+        NotifyService,
+        LogService,
+        UtilsService,
+        TokenService,
+        AbpSessionService,
+        AbpMultiTenancyService,
+        PermissionCheckerService,
+        FeatureCheckerService,
+        LocalizationService,
+        SettingService
+    ]
+})
+export class AbpModule {
+
+}
